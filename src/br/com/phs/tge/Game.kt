a53eca98469e05740737c5303f785bd396c368ae
Roger Philippe <rogerphilippe@outlook.com>
2019-12-31 03:12:11 -0300

    Instancia de handler na classe Game

--- src/br/com/phs/tge/Game.kt
+++ src/br/com/phs/tge/Game.kt
@@ -2,6 +2,8 @@ package br.com.phs.tge
 
 class Game(private val gameTitle: String, private val gameWidth: Int, private val gameHeight: Int) {
 
+    val handler = Handler(this)
+
 }
 
 class Handler(private val game: Game) {
